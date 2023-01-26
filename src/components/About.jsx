aba11a3e264ea8e04b9e6ebe3e51ca03dc1365d7
rosa ap <7266099-rosa.ap@users.noreply.gitlab.com>
2023-01-26 11:52:18 +0700

    Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import aboutImg from "../assets/images/about.png"
 
 const About = () => {
-  return <div>About</div>;
+  const info=[
+    {text:"Years experience", count:"04"},
+    {text:"Completed Project", count:"24"},
+    {text:"Companies Work", count:"06"},
+  ]
+  return(
+     <section id="about" className="py-10 text-white">
+    <div className="text-center mt-8">
+      <h3 className="text-4xl font-semibold">
+        About <span className="text-cyan-600">Me</span>
+      </h3>
+      <p className="text-gray-400 my-3 text-lg">My introduction</p>
+ 
+    <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+      <div className="p-2">
+        <div className="text-gray-300 my-3">
+          <p className="text-justify leading-7 w-11/12 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero delectus molestias explicabo, quo obcaecati, perferendis esse eum quia ipsam omnis temporibus vel. Magni ea nam at voluptatem iste aliquid.
+          </p>
+          <div className="flex mt-10 items-center gap-7">
+           { info.map(content=>(
+              <div key={content.text}>
+                <h3 className="md:text-4xl text-2xl font-semibold">{content.count}
+                <span className="text-cyan-600">+</span>{" "}
+                </h3>
+                <span className="md:text-base text-xs">{content.text}</span>
+              </div>
+            ))}
+          </div>
+          <br />
+          <br />
+          <a href="./src/assets/Code_a_program.pdf" download>
+            <button className="btn-primary ">Download CV</button>
+          </a>
+        </div>
+      </div>
+        <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg">
+              <img src={aboutImg} alt="" className="w-full object-cover bg-cyan-600 rounded-xl" />
+            </div>
+        </div>
+    </div>
+    </div>
+  </section>
+  );
 };
 
 export default About;
--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import hireMe from "../assets/images/hireMe.png";
 
 const Hireme = () => {
-  return <div>Hireme</div>;
+  return (
+    <section id="hireme" className="py-10 px-3 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-seminold">
+          Hire <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">Do you have any work?</p>
+      </div>
+      <div className="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
+        <div>
+          <h2 className="text-2xl font-semibold">
+            Do you want any work from me?
+          </h2>
+          <p className="lg:text-left text-justify max-w-lg text-sm mt-4 text-gray-200 leading-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet sed
+            dolorem unde necessitatibus placeat doloribus officia beatae
+            perferendis nobis, debitis accusantium dicta aliquam recusandae
+            voluptatem nostrum hic molestiae eius illo.
+          </p>
+          <button className="btn-primary mt-10">Say Hello</button>
+        </div>
+        <img
+          src={hireMe}
+          alt=""
+          className="lg:h-[32rem] h-80 lg:absolute bottom-0 -right-3 object-cover"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hireme;
